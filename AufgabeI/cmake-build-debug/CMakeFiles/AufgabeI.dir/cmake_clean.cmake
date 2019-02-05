75de340e847428868c5e24abaab801b9e3d46aa0
lruehle <35702288+lruehle@users.noreply.github.com>
2019-02-05 12:46:56 +0100

    start

---
+++ AufgabeI/cmake-build-debug/CMakeFiles/AufgabeI.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AufgabeI.dir/main.cpp.obj"
+  "AufgabeI.pdb"
+  "AufgabeI.exe"
+  "AufgabeI.exe.manifest"
+  "AufgabeI.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AufgabeI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
